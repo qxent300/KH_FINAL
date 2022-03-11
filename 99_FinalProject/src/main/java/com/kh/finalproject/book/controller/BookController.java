048a697cec3864ce0f9a164dfaa4be3d72d4f988
기범준 <kkel3333@gmail.com>
2022-03-11 22:09:29 +0900

    0311

--- 99_FinalProject/src/main/java/com/kh/finalproject/book/controller/BookController.java
+++ 99_FinalProject/src/main/java/com/kh/finalproject/book/controller/BookController.java
@@ -1,5 +1,24 @@
 package com.kh.finalproject.book.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.kh.finalproject.book.model.service.BookService;
+
+@RequestMapping("/book")
+@Controller
 public class BookController {
-// test입니다
+	
+	@Autowired
+	private BookService service;
+
+	@GetMapping("/search")
+	public void bookSearch() {
+	}
+	
+	@GetMapping("/view")
+	public void bookDetail() {
+	}
 }
